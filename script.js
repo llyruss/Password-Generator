1d40f1b0d07c1fa5f9382a2393f03297aee6e16a
Lily Russ <llyruss@yahoo.com>
2022-09-11 15:57:23 -0500

    concatinated arrays if selected

--- script.js
+++ script.js
@@ -13,11 +13,14 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Arrays for character types
+let charArray=[]
+let passwordArray= []
 let lowercase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"] ;
 let uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',] ;
 let numbers = [ "0", "1", "2", "3", "4", "5", "6", "7", "8","9"] ;
 let special = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
 
+let numCharTypes=0 
 
 function generatePassword() {
 
@@ -27,25 +30,37 @@ function generatePassword() {
   let isLowercase= confirm("Do you want to include lowercase letters? Click OK for yes and cancel for no.")
     if(isLowercase===true) {
       isCharTypeSelected = true
+      numCharTypes++
+      charArray=charArray.concat(lowercase)
     }
+
 
   //prompt for uppercase
   let isUppercase= confirm("Do you want to include uppercase letters? Click OK for yes and cancel for no.")
     if(isUppercase===true){
       isCharTypeSelected = true
+      numCharTypes++
+      charArray=charArray.concat(uppercase)
     }
 
   //prompt for numerical characters
   let hasNumbers= confirm("Do you want to include numbers in your password? Click OK for yes and cancel for no.")
 if(hasNumbers===true){
   isCharTypeSelected = true
+  numCharTypes++
+  charArray=charArray.concat(numbers)
 }
 
   //prompt for special characters
   let hasSpecial= confirm("Do you want to special characters? Click OK for yes and cancel for no.")
   if(hasSpecial===true){
   isCharTypeSelected = true
+  numCharTypes++
+  charArray=charArray.concat(special)
 }
+
+console.log(charArray)
+
 if(isCharTypeSelected===false){
 alert("At least one character type must be selected.")
 generatePassword()
